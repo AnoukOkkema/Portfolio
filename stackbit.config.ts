d5bf019fe40d01def1ef43c749d3fd92a69fd023
AnoukOkkema <89927283+PsDragon@users.noreply.github.com>
2024-06-02 22:21:57 +0200

    delete some js and add stackbit

---
+++ stackbit.config.ts
@@ -0,0 +1,14 @@
+// stackbit.config.ts
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  stackbitVersion: "~0.6.0",
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["src/pages"],
+      models: []
+    })
+  ]
+});
